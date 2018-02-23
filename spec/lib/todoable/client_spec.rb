4af8391c977b2990eae2aaed6e5a96297968bcc6
Edwin Mak <emak@Edwins-MacBook-Pro.local>
2018-02-23 06:56:06 -0500

    Transfer init test to rspec

--- spec/lib/todoable/client_spec.rb
+++ spec/lib/todoable/client_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
-describe "something" do
-  context "with something false" do
-    subject { false }
-    it { should_not be}
+describe Todoable::Client do
+  let(:client) { described_class.new(username, password)}
+  let(:username) { 'Buck' }
+  let(:password) { 'Mason' }
+
+  describe "initialize" do
+    context "when given no username or password" do
+      it "raises an argument error" do
+        expect { described_class.new }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when given a username and password" do
+      subject { client }
+      it { should be_a(Todoable::Client) }
+    end
   end
 end
